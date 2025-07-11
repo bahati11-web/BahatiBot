f1ebee30b6c608041873aa2caef4bc4cd71578fa
bahati11-web <bahatibot@gmail.com>
2025-07-11 15:19:55 +0100

    Update user-config.py

--- user-config.py
+++ user-config.py
@@ -1,16 +1,4 @@
-# user-config.py — Configuration pour Pywikibot
-
+usernames['wikipedia']['fr'] = 'Bahatispam'
 family = 'wikipedia'
 mylang = 'fr'
-usernames['wikipedia']['fr'] = 'Bahatispam'
-
-# Encodage de la console (utile pour éviter les erreurs Unicode)
 console_encoding = 'utf-8'
-
-# Ne pas interagir avec les autres wikis pour éviter des erreurs
-# (utile surtout si ton bot est mono-wiki)
-multilang_sites = False
-
-# Evite les questions interactives (si True)
-# Non indispensable, à activer si ton bot est autonome
-# assume_yes = True
